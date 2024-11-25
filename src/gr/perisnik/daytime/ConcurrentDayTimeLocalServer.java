5a49522a1aa0aa76aa42e5f28a58059304510db1
perisnik <nikouliciousp@gmail.com>
2024-11-25 19:08:09 +0200

    v1.7 Update DayTimeClientForLocalServer

--- src/gr/perisnik/daytime/ConcurrentDayTimeLocalServer.java
+++ src/gr/perisnik/daytime/ConcurrentDayTimeLocalServer.java
@@ -1,12 +1,12 @@
 package gr.perisnik.daytime;
 
-        import java.io.BufferedWriter;
-        import java.io.IOException;
-        import java.io.OutputStreamWriter;
-        import java.net.InetSocketAddress;
-        import java.net.ServerSocket;
-        import java.net.Socket;
-        import java.util.Date;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
 /**
  * ConcurrentDayTimeServer is a simple server program that listens for incoming connections
